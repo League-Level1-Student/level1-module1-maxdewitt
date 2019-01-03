6dbbba67cbf89ac76cf32c4996677f49042ae70c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-02 16:55:28 -0800

    Duck

---
+++ src/Duck.java
@@ -0,0 +1,18 @@
+
+public class Duck {
+
+	int numberOfFriends;
+	String favoriteFood;
+	
+	void quack() {
+		System.out.println("Quack");
+	}
+	void waddle() {
+		System.out.println("Waddle");	}
+	
+	Duck(String favoriteFood, int numberOfFriends) {
+	       this.favoriteFood = favoriteFood;
+	       this.numberOfFriends = numberOfFriends;
+	}
+	
+}
